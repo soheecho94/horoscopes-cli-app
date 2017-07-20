1f801540b7af20af249702bfb8121b70bf064ea0
Sohee Cho <soheecho94@gmail.com>
2017-07-20 12:03:21 +0000

    refactor code

--- lib/horoscopes/cli.rb
+++ lib/horoscopes/cli.rb
@@ -5,6 +5,7 @@ class Horoscopes::CLI
   def call
     puts "Welcome to daily horoscopes!"
     Horoscopes::Scraper.new.signs
+    Horoscopes::Scraper.new.reading
     list_zodiac
     main_menu
     goodbye
@@ -18,10 +19,10 @@ class Horoscopes::CLI
 
   def main_menu
     puts "Please enter the number of your zodiac sign from the list above:"
-      readings = Horoscopes::Readings.reading
       input = gets.strip.to_i
-      if input <= Horoscopes::Zodiacs.signs.size
-        readings[input - 1].reading
+      if input <= Horoscopes::Zodiacs.all.size
+        puts "Today's Reading:"
+        Horoscopes::Zodiacs.all[input - 1].reading
         horoscope_reading
       else
         puts "Looks like that doesn't exist in the stars..."
@@ -29,70 +30,13 @@ class Horoscopes::CLI
       end
   end
 
-    # hard-code:
-    # def main_menu
-    #   puts "Please enter the number of your zodiac sign from the list above:"
-    #   input = gets.strip.to_i
-    #   if input == 1
-    #     puts "Today's reading:"
-    #     puts Horoscopes::Readings.aries
-    #     horoscope_reading
-    #   elsif input == 2
-    #     puts "Today's reading:"
-    #     puts Horoscopes::Readings.taurus
-    #     horoscope_reading
-    #   elsif input == 3
-    #     puts "Today's reading:"
-    #     puts Horoscopes::Readings.gemini
-    #     horoscope_reading
-    #   elsif input == 4
-    #     puts "Today's reading:"
-    #     puts Horoscopes::Readings.cancer
-    #     horoscope_reading
-    #   elsif input == 5
-    #     puts "Today's reading:"
-    #     puts Horoscopes::Readings.leo
-    #     horoscope_reading
-    #   elsif input == 6
-    #     puts "Today's reading:"
-    #     puts Horoscopes::Readings.virgo
-    #     horoscope_reading
-    #   elsif input == 7
-    #     puts "Today's reading:"
-    #     puts Horoscopes::Readings.libra
-    #     horoscope_reading
-    #   elsif input == 8
-    #     puts "Today's reading:"
-    #     puts Horoscopes::Readings.scorpio
-    #     horoscope_reading
-    #   elsif input == 9
-    #     puts "Today's reading:"
-    #     puts Horoscopes::Readings.sagittarius
-    #     horoscope_reading
-    #   elsif input == 10
-    #     puts "Today's reading:"
-    #     puts Horoscopes::Readings.capricorn
-    #     horoscope_reading
-    #   elsif input == 11
-    #     puts "Today's reading:"
-    #     puts Horoscopes::Readings.aquarius
-    #     horoscope_reading
-    #   elsif input == 12
-    #     puts "Today's reading:"
-    #     puts Horoscopes::Readings.pisces
-    #     horoscope_reading
-    #   else
-    #     puts "Looks like that doesn't exist in the stars..."
-    #     main_menu
-    #   end
-    # end
-
   def horoscope_reading
     puts "Please type menu for list of zodiac signs or exit to leave:"
     input = ""
     while input != "exit"
       input = gets.strip.downcase
-      if input == "menu"
+      case input
+      when "menu"
         list_zodiac
         main_menu
       else
--- lib/horoscopes/scraper.rb
+++ lib/horoscopes/scraper.rb
@@ -7,9 +7,17 @@ class Horoscopes::Scraper
 
       sign.name = signs.css("h3").text
       sign.number = signs.css("a").attribute('href').value[-1]
-      sign.reading = nil
 
       sign.save
+    end
+  end
+
+  def reading
+    Horoscopes::Zodiacs.all.each do |sign|
+        number = sign.number
+        doc = Nokogiri::HTML(open("https://www.horoscope.com/us/horoscopes/general/horoscope-general-daily-today.aspx?sign=#{number}"))
+        sign_reading = doc.css("div.horoscope-content p:first-child").text.strip
+        sign.reading = sign_reading
     end
   end
 
